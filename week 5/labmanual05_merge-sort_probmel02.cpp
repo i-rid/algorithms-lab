b31307165858cbf6d95f2dd598152f0866664911
Ridwan Ahamad <62206988+i-rid@users.noreply.github.com>
2020-08-10 14:44:34 +0600

    Create labmanual05_merge-sort_probmel02.cpp

---
+++ week 5/labmanual05_merge-sort_probmel02.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(int a[], int l, int m, int r)
+{
+    int i, j, k;
+    int num1 = m - l + 1;
+    int num2 = r - m;
+    int L[num1], R[num2];
+    for (i = 0; i < num1; i++)
+    {
+        L[i] = a[l + i];
+    }
+    for (j = 0; j < num2; j++)
+    {
+        R[j] = a[m + 1+ j];
+    }
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < num1 && j < num2)
+    {
+        if (L[i] <= R[j])
+        {
+            a[k] = L[i];
+            i++;
+        }
+        else
+        {
+            a[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < num1)
+    {
+        a[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < num2)
+    {
+        a[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int a[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l+(r-l)/2;
+        mergeSort(a, l, m);
+        mergeSort(a, m+1, r);
+        merge(a, l, m, r);
+    }
+}
+void printaay(int A[], int size)
+{
+    int i;
+    int temp,temp2,digsum=0,sum=0;
+
+    for (i=0; i < size; i++)
+    {
+        printf("%d ", A[i]);
+        sum+=A[i];
+    }
+    temp=sum;
+    while(temp)
+    {
+        temp2=temp%10;
+        digsum+=temp2;
+        temp=temp/10;
+    }
+    printf("\n");
+    printf("Sum %d\n",sum  );
+    printf("Digit Sum %d\n",digsum);
+}
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int a[1000], array_size;
+    printf("Enter The size of the Array: ");
+    scanf("%d", &array_size);
+    printf("Enter Elements of Array : \n");
+    for(int i = 0; i < array_size; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    mergeSort(a, 0, array_size - 1);
+    printf("\nSorted Array is : ");
+    printaay(a, array_size);
+
+    return 0;
+}
